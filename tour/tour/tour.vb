f5eb3f48103fe30e69cad6512949e402b9aee4c6
pietro-mesch <mezkaster@gmail.com>
2017-01-19 12:40:15 +0100

    increased roster size

--- tour/tour/tour.vb
+++ tour/tour/tour.vb
@@ -4,7 +4,7 @@
         Dim m As Integer = 100
         Dim match As Match
 
-        Dim r As New Roster(10)
+        Dim r As New Roster(15)
 
         For i As Integer = 1 To m
             match = r.RandomMatch
